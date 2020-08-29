b8ed5da2839f34e0ed5a31f7c5db37879880eab1
yuroupan <yrpan@jinshuju.net>
2020-08-29 15:29:56 +0800

    add email deliver

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit(:username, :password))
     if @user.save
+      UserMailer.welcome_email(@user).deliver_later
       #flash 是一次性的闪现信息，之后用户重新刷新页面等操作不会再出现
       flash[:notice] = "注册成功请登录"
       redirect_to new_session_path
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,17 @@ module Circles
     config.load_defaults 6.0
     config.autoload_paths << "#{Rails.root}/app/reports"
 
+    # 以smtp邮件传送协议发送邮件
+    config.action_mailer.delivery_method = :smtp
+    # 邮件服务器的设置
+    config.action_mailer.smtp_settings = {
+        :address              => "smtp.gmail.com",
+        :port                 => 587,
+        :user_name            => 'userName', #不要写成了邮件,比如userName@gamil.com 这个写userName
+        :password             =>  "password",
+        :authentication       => 'plain',
+        :enable_starttls_auto => true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
